27e4498329cbcc50423be07a433a44c68af37b8e
Pavel Klyuev <shketoz@gmail.com>
2023-02-27 00:52:31 +0300

    init

--- script.js
+++ script.js
@@ -260,13 +260,90 @@ const eventFunc = (e) => {
     // counterEl.innerText = +counterEl.innerText +1
     if (e.target.className === "") {
         e.target.className = "choosed_element"
-    choosedState.setCountValue (1)
+        choosedState.setCountValue(1)
     } else {
         e.target.className = ""
-        choosedState.setCountValue (-1)
+        choosedState.setCountValue(-1)
     }
 }
 
 for (let i = 0; i < choosedEl.length; i++) {
     choosedEl[i].addEventListener("click", eventFunc)
 }
+
+// const timeIsOver = () => {
+//     alert("Время вышло!")
+// }
+
+// const alarm = setInterval(() => {
+// let wantToSleep = confirm("Хотите ли вы спать?")
+//     if (wantToSleep) {
+//         console.log("tic")
+//     } else {
+//         clearInterval(alarm)
+//     }
+// }, 3000)
+
+
+// const alarm = setInterval(timeIsOver, 3000)
+// setTimeout(timeIsOver, 2000)
+
+// console.log(1)
+// setTimeout(() => {
+//     console.log(2)} , 0)
+// console.log(3)
+
+const postBlock = document.querySelector(".posts_block-container")
+const showPostsBTN = document.querySelector(".posts_block button")
+
+
+function addPost(title, body) {
+    const postTitle = document.createElement("h3")
+    const postBody = document.createElement("span")
+    const postItem = document.createElement("p")
+
+    postTitle.innerText = title
+    postBody.innerText = body
+
+    postItem.append(postTitle, postBody)
+    postBlock.append(postItem)
+}
+
+function getPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => {
+            for (item of data) {
+                addPost(item.title, item.body)
+            }
+        })
+        .catch(err => console.log(err.message))
+}
+
+// function createPost(title, body, userId) {
+//     fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: 'POST',
+//         body: JSON.stringify({
+//             // title: title,
+//             // body: body,
+//             // userId: userId
+//             title,
+//             body,
+//             userId,
+//         }),
+//         headers: {
+//             'Content-type': 'application/json; charset=UTF-8',
+//         }
+//     })
+//         .then(res => res.json())
+//         .then(data => {
+//             for (item of data) {
+//                 addPost(item.title, item.body)
+//             }
+//         })
+//         .catch(err => console.log(err.message))
+// }
+
+// createPost("title", "body", 15)
+
+showPostsBTN.onclick = () => {getPosts()}
